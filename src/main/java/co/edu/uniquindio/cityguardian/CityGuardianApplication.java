b58c5736f3eb6b1443ad4228cbd8e44551602355
Andres-Correa69 <141978219+Andres-Correa69@users.noreply.github.com>
2025-02-14 21:15:35 -0500

    InitialCommit

---
+++ src/main/java/co/edu/uniquindio/cityguardian/CityGuardianApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.cityguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityGuardianApplication.class, args);
+    }
+
+}
